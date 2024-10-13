e0c293c122b38e2edaaea8f0ba4697b24b6e6656
nizzamiramdhan@gmail.com <nizzamiramdhan@gmail.com>
2024-10-13 10:22:15 +0700

    Add city selection (Spinner) and Note input for Module 2

--- app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
+++ app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
@@ -2,7 +2,11 @@ package lat.pam.droidcafe
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.RadioButton
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +14,43 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class OrderActivity : AppCompatActivity() {
+
+    private lateinit var noteEditText: EditText
+    private lateinit var citySpinner: Spinner
+    private var selectedCity: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_order)
+
+        noteEditText = findViewById(R.id.note)
+
+        citySpinner = findViewById(R.id.city_spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.city_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            citySpinner.adapter = adapter
+        }
+
+        citySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                selectedCity = parent.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+        }
+    }
+
+    fun submitOrder(view: View) {
+        val note = noteEditText.text.toString()
+        // Lakukan proses sesuai kebutuhan
+        Toast.makeText(this, "Note: $note, City: $selectedCity", Toast.LENGTH_LONG).show()
     }
 
     fun onRadioButtonClicked(view: View) {
